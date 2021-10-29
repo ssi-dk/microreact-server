1a53afd69b35f6df7d16331c3506cfc8d0cf669d
Khalil <ka10@sanger.ac.uk>
2021-10-29 12:32:23 +0100

    Update dataabse.js

--- services/dataabse.js
+++ services/dataabse.js
@@ -1,6 +1,18 @@
 const connect = require("cgps-application-server/db/connect");
+// const mongoose = require("mongoose");
 
 const serverRuntimeConfig = require("../utils/server-runtime-config");
+
+// mongoose.set('debug', true);
+
+// export function toObjectId(value) {
+//   if (typeof value === "string") {
+//     return new mongoose.Types.ObjectId(value);
+//   }
+//   else {
+//     return value;
+//   }
+// }
 
 export default async function connectToDatabase() {
   const db = connect({
