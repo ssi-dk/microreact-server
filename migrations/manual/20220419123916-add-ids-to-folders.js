299f4f80953fef52c9ceb0f73873377ab3661098
Khalil <ka10@sanger.ac.uk>
2022-04-19 16:05:08 +0100

    Create 20220419123916-add-ids-to-folders.js

---
+++ migrations/manual/20220419123916-add-ids-to-folders.js
@@ -0,0 +1,28 @@
+const shortUUID = require("short-uuid");
+
+module.exports = {
+
+  async up(db, client) {
+    let index = 0;
+
+    const docCursor = db.collection("folders").find();
+
+    for await (const doc of docCursor) {
+      console.log("Adding id to folder %s %s", index, doc._id);
+
+      await db.collection("folders").updateOne(
+        { _id: doc._id },
+        {
+          $set: { id: shortUUID.generate() },
+        },
+      );
+
+      index++;
+    }
+
+    await db.collection("folders").rename("collections");
+  },
+
+  async down(db, client) {},
+
+};
