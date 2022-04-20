1d43edf7295f4c490948e72ee51a23bd33e583f3
Khalil <ka10@sanger.ac.uk>
2022-04-20 11:13:34 +0100

    Update start.js

--- experimental/start.js
+++ experimental/start.js
@@ -2,10 +2,7 @@ const apiUtils = require("next/dist/next-server/server/api-utils.js");
 
 const nextApiResolver = apiUtils.apiResolver;
 // apiUtils.apiResolver = async function (...args) {
-//   console.log("custom api");
 //   return nextApiResolver.apply(null, args);
 // };
-
-// console.log(apiUtils)
 
 require("./node_modules/.bin/next");
