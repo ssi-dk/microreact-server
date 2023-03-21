3345e66bcdd4d745edf155f18f1fe7054e2bc7f2
Khalil <khalil.abudahab@cgps.group>
2023-03-21 11:52:23 +0000

    SUPPORT-46

--- models/project/methods/save-json.js
+++ models/project/methods/save-json.js
@@ -2,6 +2,10 @@ const { projectSchemaVersion } = require("../schema");
 
 const FileStorage = require("../../../services/file-storage");
 
+function base64ToBlob(base64) {
+  return fetch(base64).then((res) => res.text());
+}
+
 /**
  * Saves inline files in storage and converts them to URLs
  * @param {Array} files
@@ -9,6 +13,9 @@ const FileStorage = require("../../../services/file-storage");
 async function saveInlineFilesInStorage(projectJson) {
   for (const file of Object.values(projectJson.files || {})) {
     if (file.blob && typeof file.blob === "string") {
+      if (/^data:.*\/.*;base64,/i.test(file.blob)) {
+        file.blob = await base64ToBlob(file.blob);
+      }
       const fileHash = await FileStorage.storeText(file.blob);
       file.blob = undefined;
       file.url = FileStorage.uri(fileHash);
