fe7f03ed7e3ca09f7f255713f328f8836d929355
Khalil <khalil.abudahab@cgps.group>
2023-06-19 10:42:24 +0100

    Update raw.js

--- pages/api/files/raw.js
+++ pages/api/files/raw.js
@@ -1,6 +1,10 @@
 import { ApiError } from "next/dist/server/api-utils";
 
+import getUserMiddleware from "cgps-application-server/middleware/get-user";
+
 import FileStorage from "../../../services/file-storage";
+import * as ProjectsService from "../../../services/projects";
+import databaseService from "../../../services/dataabse";
 
 export const config = {
   api: {
@@ -9,14 +13,37 @@ export const config = {
 };
 
 export default async function (req, res) {
+  await databaseService();
+
   const fileHash = req.query.id || req.query.hash || (Object.keys(req.query)?.[0]);
   if (!fileHash) {
     throw new ApiError(400);
   }
 
+  if (!req.headers?.referer) {
+    throw new ApiError(400);
+  }
+  const refererUrl = new URL(req.headers.referer);
+
+  const user = await getUserMiddleware(req, res);
+  const parts = refererUrl.pathname.split("/");
+  const projectModel = await ProjectsService.getProjectDocument(parts[2], user);
+  const jsonDocument = await ProjectsService.toViewerJson(projectModel);
+
+  let fileFound = false;
+  for (const fileDef of Object.values(jsonDocument.files)) {
+    if (fileDef.url && fileDef.url.endsWith(fileHash)) {
+      fileFound = true;
+      break;
+    }
+  }
+  if (!fileFound) {
+    throw new ApiError(400);
+  }
+
   const stream = await FileStorage.readStream(fileHash);
 
-  res.setHeader("Access-Control-Allow-Origin", "https://vega.github.io");
+  // res.setHeader("Access-Control-Allow-Origin", "https://vega.github.io");
   res.setHeader("Cache-control", "private, max-age=31536000, immutable");
 
   stream.pipe(res);
