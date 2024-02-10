dcb6ef65279da1d903beb127e8009638d1b0cef2
Khalil <khalil.abudahab@cgps.group>
2024-02-10 09:16:46 +0000

    TASK-2661

--- utils/projects.js
+++ utils/projects.js
@@ -1,23 +1,26 @@
-import * as Api from "./api-client";
+import { blobify } from "microreact-viewer/utils/files.js";
+import * as Api from "./api-client.js";
 
 async function tryFileAccess(fileBlob) {
   try {
-    const stream = fileBlob.stream();
+    const blob = await blobify(fileBlob);
+    const stream = blob.stream();
     const reader = stream.getReader();
     await reader.read();
     return true;
   }
   catch (err) {
     console.error(err);
-    throw new Error(`Cannot access file ${fileBlob.name} becasue it has been renamed, moved, or deleted.`);
+    throw new Error(`Cannot access file ${fileBlob.name} because it has been renamed, moved, or deleted.`);
   }
 }
 
 async function storeFilesOnServer(projectJson) {
   for (const file of Object.values(projectJson.files)) {
     if (file.blob) {
-      await tryFileAccess(file.blob, file.name);
-      const { hash, url } = await Api.storeFile(file.blob);
+      const blob = await blobify(file.blob);
+      await tryFileAccess(blob, file.name);
+      const { hash, url } = await Api.storeFile(blob);
       file.hash = hash;
       file.url = url;
       file.blob = undefined;
