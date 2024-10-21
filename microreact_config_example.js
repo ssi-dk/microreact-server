52f80cd11bbc1186c3cb95c48309d485adfb1c2e
Simon J.K. Pedersen (Delegate) <skp@delegate.dk>
2024-10-21 12:31:32 +0200

    test new funcitonality

--- microreact_config_example.js
+++ microreact_config_example.js
@@ -1,50 +1,64 @@
-module.exports = {   
-    auth: {
-      email: {
-        server: {
-          host: "mailhog",
-          port: 1025,
-          auth: {
-            user: "user",
-            pass: "test"
-          },
-          secure: false
-        },
-        from: "Microreact <noreply@microreact.org>",
-        subject: "Login link for Microreact",
-        text: "Hello!\nAccess your account here: http://localhost:80/auth/passwordless/callback?token=<%= tokenToSend %>&uid=",
-        html: "views/emails/passwordless.html"
-      },
-      secret: process.env.AUTH_SECRET,
-      session: {
-        maxAge: 2592000,
-        updateAge: 86400
-      }
+module.exports = {
+  downloadActions: [
+    {
+      id: "addtocluster",
+      label: "Add selection to cluster",
+      type: "selection",
+      url: "https://dev2.sofi-platform.dk/cluster"
     },
-    baseUrl: "https://ms.local/",
-    bodySizeLimit: "16mb",
-    experimentalFlags: {
-      publicFolders: false
-    },
-    helpDesk: {
-      email: "support@microreact.org",
-      subject: "Feedback from Microreact.org"
-    },
-    mapboxApiAccessToken: "pk.eyJ1IjoiY2dwc2RldiIsImEiOiJja2lhMndmN3AwbGpzMnhvNWZ0eWc3cWhoIn0.SDSaZCcDFP9s812jX1tzeA",
-    mongodb: {
-      url: process.env.MONGODB_CONNECTION,
-      database: process.env.MONGODB_DATABASE
-    },
-    repoPath: "./files",
-    showcaseFolders: [],
-    smtp: {
-      host: "mailhog",
-      port: 1025,
-      auth: {
-        user: "user",
-        pass: "test"
-      },
-      secure: false,
-      from: "Microreact <noreply@microreact.org>"
+    {
+      id: "createnewworkspace",
+      label: "Create new workspace from selection",
+      type: "selection",
+      url: "https://dev2.sofi-platform.dk/workspaces/new"
     }
-  };
+  ],
+  auth: {
+    email: {
+      server: {
+        host: "mailhog",
+        port: 1025,
+        auth: {
+          user: "user",
+          pass: "test"
+        },
+        secure: false
+      },
+      from: "Microreact <noreply@microreact.org>",
+      subject: "Login link for Microreact",
+      text: "Hello!\nAccess your account here: http://localhost:80/auth/passwordless/callback?token=<%= tokenToSend %>&uid=",
+      html: "views/emails/passwordless.html"
+    },
+    secret: process.env.AUTH_SECRET,
+    session: {
+      maxAge: 2592000,
+      updateAge: 86400
+    }
+  },
+  baseUrl: "https://ms.local/",
+  bodySizeLimit: "16mb",
+  experimentalFlags: {
+    publicFolders: false
+  },
+  helpDesk: {
+    email: "support@microreact.org",
+    subject: "Feedback from Microreact.org"
+  },
+  mapboxApiAccessToken: "pk.eyJ1IjoiY2dwc2RldiIsImEiOiJja2lhMndmN3AwbGpzMnhvNWZ0eWc3cWhoIn0.SDSaZCcDFP9s812jX1tzeA",
+  mongodb: {
+    url: process.env.MONGODB_CONNECTION,
+    database: process.env.MONGODB_DATABASE
+  },
+  repoPath: "./files",
+  showcaseFolders: [],
+  smtp: {
+    host: "mailhog",
+    port: 1025,
+    auth: {
+      user: "user",
+      pass: "test"
+    },
+    secure: false,
+    from: "Microreact <noreply@microreact.org>"
+  }
+};
